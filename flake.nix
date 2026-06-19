5905fc3418ef389fe9a547b8635430368a55fd4a
yutakobayashidev <hi@yutakobayashi.com>
2026-06-19 22:07:23 +0900

    feat: add nixpkgs input and devShell with convco

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Project templates collection";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     moonbit-overlay.url = "github:moonbit-community/moonbit-overlay";
     web-app-template = {
@@ -13,7 +14,20 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ ];
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShellNoCC {
+            packages = with pkgs; [ convco ];
+          };
+        };
 
       flake = {
         templates = import ./template.nix { inherit inputs; };
